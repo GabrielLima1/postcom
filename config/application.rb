5a7998f73b918245cacc53fea82d91269ffef353
Gabriel Lima <gabriellimatrajano@hotmail.com>
2017-08-07 00:04:23 -0300

    fix pt 2

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,6 @@ module Postcom3
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Brasilia'
-
     config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}").to_s]
     config.i18n.default_locale = "pt-BR"
     config.encoding = "utf-8"
