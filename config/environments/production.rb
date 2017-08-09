f9289fa9c03476add3d1a2efd63fcf4f60677092
Gabriel Lima <gabriellimatrajano@hotmail.com>
2017-08-09 16:41:54 -0300

    test email send

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,6 +59,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "postcom3_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
+  config.mailer_sender = 'PostCom <nao-responda@postcom.com.br>'
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: ENV["EMAIL_USER_NAME"]}
   config.action_mailer.delivery_method = :smtp
