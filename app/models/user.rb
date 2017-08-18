7dba1249f4a4e9542ea422db438ee5cacfb49e83
Gabriel Lima <gabriellimatrajano@hotmail.com>
2017-08-17 23:55:48 -0300

    alter form user, and form company

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :trackable, :validatable
-  validates :name, presence: true
+  #validates :name, presence: true
   after_create :email_admin_user
 
   enum kind: [:client, :manager]
